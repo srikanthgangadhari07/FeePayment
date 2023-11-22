fdbe494578110c3fd4b384f23ea96de7192600ea
Maneesh Murali <maneeshmurali@Maneeshs-MacBook-Air.local>
2023-11-22 08:36:29 +0530

    cors origin

--- src/main/java/com/payment/service/PaymentController.java
+++ src/main/java/com/payment/service/PaymentController.java
@@ -2,6 +2,7 @@ package com.payment.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("api/v1")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:8026")
 public class PaymentController {
 	
 	@Autowired
